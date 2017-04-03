17bd5059c6a8f7d7b5dbb86748d22c4413d0c4c9
PaulBender <paulbender@live.com>
2017-04-03 19:35:11 -0400

    Removed PHP closing tags

--- inc/languages/english/closethread.lang.php
+++ inc/languages/english/closethread.lang.php
@@ -6,5 +6,3 @@
 
 $l['closethread_info_name'] = "Close Thread At Reply Count";
 $l['closethread_info_desc'] = "Allows you to have threads automatically closed upon reaching a certain amount of replies.";
-
-?>
