2b0af555300f4e8195d957779cc5c7fc3b07e4ac
Paul Bender <paulbender@live.com>
2021-06-23 14:54:31 -0400

    Bump to version v1.2

--- inc/plugins/closethread.php
+++ inc/plugins/closethread.php
@@ -25,7 +25,7 @@ function closethread_info()
 		"website"			=> "http://galaxiesrealm.com/index.php",
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
-		"version"			=> "1.1",
+		"version"			=> "1.2",
 		"codename"			=> "closethread",
 		"compatibility"		=> "18*"
 	);
